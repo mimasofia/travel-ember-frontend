263c633c9d780a182e9296c49d63905ae055694d
mimasofia <virginiadonaire@hotmail.com>
2018-03-08 10:57:59 -0500

    Fixes multiple issues
    First adds a component called `travel-list` that now houses the items
    and the save and delete actions for that item. Had to be separated
    because in the `place` there was an issue when trying to add items from
    there
    
    Second removes the RSVP.hash from `place` because it was bypassing the
    model hook so that when you clicked on the title to take you to a new
    page to display info for that `place` it would not load unless refreshed
    commented out the actions related to items directly
    
    Third moved the display of the items from `place` to `travel-list` to be
    able to work on the items without interrupting the `place`
    
    Had to change `place` to `model` in various places because place was
    undefined.

--- app/components/travel-list.js
+++ app/components/travel-list.js
@@ -1,4 +1,19 @@
 import Component from '@ember/component';
 
 export default Component.extend({
+  newItem: {},
+actions: {
+  save (place) {
+      console.log('THIS MADE IT')
+      const newThing = this.get('newItem')
+      console.log('PLACE IN TRAVEL', place)
+      // what to associate this new item with the list
+      newThing.place = this.get('place')
+      this.sendAction('addItem', newThing)
+    },
+      deleteItem (item) {
+        // console.log('item', item)
+        return item.destroyRecord()
+      }
+  }
 });
--- app/routes/place.js
+++ app/routes/place.js
@@ -2,14 +2,13 @@ import Route from '@ember/routing/route';
 
 export default Route.extend({
   model (params) {
-    return Ember.RSVP.hash({
-      place: this.get('store').findRecord('place', params.place_id),
-      newItem: {itemName: null}
-      // items: this.get('store').findAll('item')
-    })
-
-    // const id = +params.place_id
-    // return this.get('store').findRecord('place', id)
+    // return Ember.RSVP.hash({
+    //   place: this.get('store').findRecord('place', params.place_id),
+    //   newItem: {itemName: null}
+    //   // items: this.get('store').findAll('item')
+    // })
+    const id = +params.place_id
+    return this.get('store').findRecord('place', id)
   },
   actions: {
     delete (place) {
@@ -17,17 +16,22 @@ export default Route.extend({
       return place.destroyRecord()
         .then(() => this.transitionTo('places'))
     },
-      deleteItem (item) {
-        // console.log('item', item)
-        return item.destroyRecord()
-      },
-      saveItem (newItem, place) {
-        // console.log('Save Item')
-        console.log('newITEM', newItem)
-
-        newItem.place = place
-        const item = this.get('store').createRecord('item', newItem)
-        return item.save()
-      }
+      // deleteItem (item) {
+      //   // console.log('item', item)
+      //   return item.destroyRecord()
+      // },
+      // // saveItem (newItem, place) {
+      //   // console.log('Save Item')
+      //   console.log('newITEM', newItem)
+      //
+      //   newItem.place = place
+      //   const item = this.get('store').createRecord('item', newItem)
+      //   return item.save()
+      // }
+      addItem(newIten) {
+        console.log('in place for items')
+      const item = this.get('store').createRecord('item', newIten)
+      return item.save()
+    }
   }
 });
